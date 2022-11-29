29a90f42c8a4403f2a2e49dd250b90fa66d1b990
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2022-11-29 19:06:29 -0300

    mudando nome

--- calculadora.java
+++ calculadora.java
@@ -1,8 +1,33 @@
-public class calculadora {
-    public static void main(String[] args) {
-        operacoes operar = new operacoes();
-
-        operar.soma();
-        
-    }
+public class Calculadora {
+	public double soma() {
+		return 0 ;
+	}
+	
+	public double subtracao() {
+		return 0 ;
+	}
+	
+	public double multiplicacao() {
+		return 0 ;
+	}
+	
+	public double divisao() {
+		return 0 ;
+	}
+	
+	public double exponencial() {
+		return 0 ;
+	}
+	
+	public double raizQuadrada() {
+		return 0 ;
+	}
+	
+	public double piso() {
+		return 0 ;
+	}
+	
+	public double teto() {
+		return 0 ;
+	}    
 }
---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+    
+        calculadora.soma();
+           
+    }
+}
