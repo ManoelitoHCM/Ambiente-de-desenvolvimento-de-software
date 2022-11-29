62f2af23de74cd830af98cff7fac1b4bc94e1dd4
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2022-11-29 10:35:38 -0300

    declaração das funções

--- calculadora.java
+++ calculadora.java
@@ -1,7 +1,7 @@
 public class calculadora {
     public static void main(String[] args) {
-        operacoes Calculadora = new operacoes();
-
+        operacoes operar = new operacoes();
+        
         
     }
 }
--- operacoes.java
+++ operacoes.java
@@ -1,10 +1,33 @@
 public class operacoes {
-	public double soma(double numeroA, double numeroB) {
-		return numeroA + numeroB;
+	public double soma() {
+		return 0 ;
 	}
 	
-	public double subtracao(double numeroA, double numeroB) {
-		return numeroA - numeroB;
+	public double subtracao() {
+		return 0 ;
 	}
-    
+	
+	public double multiplicacao() {
+		return 0 ;
+	}
+	
+	public double divisao() {
+		return 0 ;
+	}
+	
+	public double exponencial() {
+		return 0 ;
+	}
+	
+	public double raizQuadrada() {
+		return 0 ;
+	}
+	
+	public double piso() {
+		return 0 ;
+	}
+	
+	public double teto() {
+		return 0 ;
+	}    
 }
