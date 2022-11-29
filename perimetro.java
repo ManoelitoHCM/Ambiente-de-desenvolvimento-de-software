4c67df1b42390ba6520fbb8ba21c7b8ccd85ac74
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2022-11-29 19:27:37 -0300

    implementando perimetro

---
+++ perimetro.java
@@ -0,0 +1,10 @@
+import Calculadora;
+
+public class perimetro {
+
+    Calculadora calculadora = new Calculadora();
+
+    public double PerimetroRetangulo(double lado1, double lado2) {
+		return calculadora.soma(lado1, lado2);
+	}
+}
