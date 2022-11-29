e54e30c4908f240ef0a98399ff2ef30c244b620f
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2022-11-29 20:12:10 -0300

    modif imc e perimetro

--- src/perimetro.java
+++ src/perimetro.java
@@ -7,6 +7,6 @@ public class perimetro {
     Calculadora calculadora = new Calculadora();
 
     public double PerimetroRetangulo(double lado1, double lado2) {
-		return calculadora.soma(lado1, lado2);
+		  return calculadora.soma(lado1, lado2);
 	}
 }
